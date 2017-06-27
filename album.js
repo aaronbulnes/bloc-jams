98cb5408acb70ae808030c3726869e74d6c748c0
aaronbulnes <aaronchrisbulnes@gmail.com>
2017-06-27 16:55:28 -0500

    checkpoint 11 finished

--- album.js
+++ album.js
@@ -12,7 +12,7 @@
          { title: 'Magenta', duration: '2:15'}
      ]
  };
- 
+
  var albumMarconi = {
      title: 'The Telephone',
      artist: 'Guglielmo Marconi',
@@ -27,7 +27,22 @@
          { title: 'Wrong phone number', duration: '2:15'}
      ]
  };
- 
+
+ var albumExample = {
+     title: 'Example
+     artist: 'Placeholder',
+     label: 'EM',
+     year: '2017',
+     albumArtUrl: 'assets/images/album_covers/17.png',
+     songs: [
+         { title: 'Track One?', duration: '1:01' },
+         { title: 'Track Two' , duration: '5:01' },
+         { title: 'Track Three', duration: '3:21'},
+         { title: 'Track Four', duration: '3:14' },
+         { title: 'Track Five', duration: '2:15'}
+     ]
+ };
+
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
@@ -36,30 +51,42 @@
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
       ;
- 
+
      return template;
  };
- 
- var setCurrentAlbum = function(album) {
-     
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
+
+	 var albumTitle = document.getElementsByClassName('album-view-title')[0];
      var albumArtist = document.getElementsByClassName('album-view-artist')[0];
      var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
      var albumImage = document.getElementsByClassName('album-cover-art')[0];
      var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
- 
-     albumTitle.firstChild.nodeValue = album.title;
+
+ var setCurrentAlbum = function(album) {
+
+  	 albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
      albumImage.setAttribute('src', album.albumArtUrl);
- 
+
      albumSongList.innerHTML = '';
- 
-     for (var i = 0; i < album.songs.length; i++) {
-         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+
+     for (i = 0; i < album.songs.length; i++) {
+
+     	albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].name, album.songs[i].length);
      }
  };
- 
+
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+
+     var albums = [albumPicasso, albumMarconi, albumExample];
+     var index = 1;
+
+     albumImage.addEventListener("click", function(event) {
+     		setCurrentAlbum(albums[index]);
+     		index++;
+     		if (index == albums.length) {
+     		index = 0;
+     	}
+     });
  };
